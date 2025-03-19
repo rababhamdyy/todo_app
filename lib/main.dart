30969e0274f161115536b5d4bb340aae2ab32fbf
rabab_hamdyy <rababhamdy38@gmail.com>
2025-03-19 21:57:55 +0200

    init Hive in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:todo_app/data/task_model.dart';
 import 'package:todo_app/pages/home_page.dart';
 
-void main() {
+void main() async{
+  await Hive.initFlutter();
+  Hive.registerAdapter(TaskModelAdapter());
+  await Hive.openBox<TaskModel>('tasks');
   runApp(const MyApp());
 }
 
